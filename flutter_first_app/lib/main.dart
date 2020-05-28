bd645386262f18dbfea35d5b99f2bf6fefa05333
Anand Raj <anandallen95@gmail.com>
2020-05-28 21:55:41 +0530

    random words added

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
+  
     return MaterialApp(
       title: 'Basic App',
       home: Scaffold(
@@ -12,12 +15,26 @@ class MyApp extends StatelessWidget {
           title: Text(' Basic App'),
            ),
            body: Center(
-             child: Text('Hello Anand!'),
+            
+             child: RandomWords(),
              ),
       ),
 
     );
   }
+}
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+
+}
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+
 }
 
 
